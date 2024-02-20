2bed9c5957985801b28fcda23d035206aff853e8
Flavio Conte <flaviohconte@gmail.com>
2024-02-20 14:57:32 -0300

    chore: added get and post users routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 const http = require('node:http')
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method == 'GET' && url == '/users') {
+    return res.end('Listagem de usuários')
+  }
+
+  if (method == 'POST' && url == '/users') {
+    return res.end('Criação de usuário')
+  }
+
   return res.end('Nexus Test')
 })
 
